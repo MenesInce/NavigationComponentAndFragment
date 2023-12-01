a604fec091a5282a2225f7827d613ec5fa2f3b1d
MENEŞ <inceem01@gmail.com>
2023-12-01 11:42:12 +0300

    Bottom Navigation (Menu)

--- app/src/main/java/com/menesince/navigationcomponentandfragment/MainActivity.kt
+++ app/src/main/java/com/menesince/navigationcomponentandfragment/MainActivity.kt
@@ -2,10 +2,22 @@ package com.menesince.navigationcomponentandfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.menesince.navigationcomponentandfragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment1 = supportFragmentManager.findFragmentById(R.id.navHostFragment1) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navHostFragment1.navController)
+
+
+
+
     }
 }
